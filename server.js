7ba2e31866bb67a8a4fde02b4ea3390ad5c68d0c
Olivia <oliviachang29@gmail.com>
2019-07-20 14:55:30 -0700

    gets directions

--- server.js
+++ server.js
@@ -2,28 +2,52 @@ const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const nearestAddress = require('./lib/nearestAddress')
+const getDirections = require('./lib/getDirections')
+const moreInfo = require('./lib/moreInfo')
 const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.post('/sms', (req, res) => {
 	(async() => {
-	  console.log('before start');
-
+		console.log('received message')
 	  // get message body
 		const messageBody = req.body.Body;
 
 		const twiml = new MessagingResponse();
 
-		const response = await nearestAddress(messageBody);
-		twiml.message(response);
+		if (req.cookies.cachedBathrooms !== undefined && !isNaN(messageBody)) {
+			console.log('cached bathrooms')
+			console.log(req.cookies.cachedBathrooms)
+
+			const selectedBathroom = req.cookies.cachedBathrooms[messageBody];
+			const response = moreInfo(selectedBathroom)
+			twiml.message(response)
+
+			res.cookie('selectedBathroom', selectedBathroom, { maxAge: 1000 * 60 * 60 });
+		} else if (req.cookies.selectedBathroom !== undefined && messageBody === "DIR") {
+			console.log("Getting directions.")
+			const destination = req.cookies.selectedBathroom['address']
+			const response = await getDirections(req.cookies.currentLocation, destination);
+			twiml.message(response.text);
+		} else {
+			console.log('getting nearest address')
+			const response = await nearestAddress(messageBody);
+			if (response.numBathrooms > 0) {
+				res.cookie('cachedBathrooms', response.bathrooms, { maxAge: 1000 * 60 * 60 });
+				res.cookie('currentLocation', response.currentLocation, { maxAge: 1000 * 60 * 60 });
+			}
+
+			twiml.message(response.text);
+		}
 
 		res.writeHead(200, {'Content-Type': 'text/xml'});
 		res.end(twiml.toString());
 	  
-	  console.log('after start');
 	})();
 });
 
--- test.js
+++ test.js
@@ -1,10 +1,15 @@
 const nearestAddress = require('./lib/nearestAddress')
-var currentLocation = 'Union Square';
+const moreInfo = require('./lib/moreInfo')
+const getDirections = require('./lib/getDirections')
+const currentLocation = "Powell and Market";
+const destination = "170 O'Farrell Street, 6th Floor"
 
 async function asyncCall() {
-  console.log('calling');
   const response = await nearestAddress(currentLocation);
-  console.log(response);
+  console.log(response.text)
+  console.log(moreInfo(response.bathrooms[0]))
+  // const response = await getDirections(currentLocation, destination);
+  // console.log(response)
 }
 
 asyncCall();
